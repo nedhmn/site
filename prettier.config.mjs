c36f980d23561014ddaca5f68305ede64b801fd1
nedhmn <hermannned@gmail.com>
2025-05-21 20:30:23 -0400

    feat: prettier-plugin-tailwindcss

---
+++ prettier.config.mjs
@@ -0,0 +1,14 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+  trailingComma: "es5",
+  semi: false,
+  singleQuote: true,
+  plugins: ["prettier-plugin-tailwindcss"],
+  tailwindFunctions: ["clsx", "cn"],
+  tailwindStylesheet: "./src/app/globals.css",
+};
+
+export default config;
