da5a399b6edfe5f1086857e9c50003f6e95afb87
nedhmn <hermannned@gmail.com>
2025-05-21 23:29:29 -0400

    feat: home summary

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,5 +1,23 @@
 const Page = () => {
-  return <div>Hello, World!</div>
-}
+  return (
+    <div>
+      <section>
+        <p className="leading-snug">
+          {`I'm a full-stack software developer with over four years of experience, 
+        primarily in agile startups and smaller companies.`}
+        </p>
+        <p className="mt-3 leading-snug">
+          {`My early startup roles
+        allowed me to wear many hats in fast-paced environments, leveraging great
+        mentors across full-stack web development, data engineering, and small team
+        leadership.`}
+        </p>
+      </section>
+      <section className="mt-10">
+        <h1 className="font-medium">Recent Projects</h1>
+      </section>
+    </div>
+  );
+};
 
-export default Page
+export default Page;
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,23 +1,22 @@
-import Link from 'next/link'
+import Link from "next/link";
 
 const PAGES = [
-  { name: 'Ned Hermann', path: '/' },
-  { name: 'blogs', path: '/blogs' },
-  { name: 'projects', path: '/projects' },
-]
+  { name: "Ned Hermann", path: "/" },
+  { name: "blogs", path: "/blogs" },
+  { name: "projects", path: "/projects" },
+];
 
 export const Header = () => {
   return (
-    <div className="my-10 flex w-[60ch] flex-row justify-between">
-      <div>
-        <Link
-          key={PAGES[0].name}
-          href={PAGES[0].path}
-          className="font-semibold text-black transition-colors duration-200 hover:text-blue-500 dark:text-white"
-        >
-          {PAGES[0].name}
-        </Link>
-      </div>
+    <header className="my-10 flex w-[60ch] flex-row items-center">
+      <Link
+        key={PAGES[0].name}
+        href={PAGES[0].path}
+        className="text-black transition-colors duration-200 hover:text-blue-500 dark:text-white"
+      >
+        {PAGES[0].name}
+      </Link>
+      <div className="flex-1" />
       <div className="flex flex-row space-x-4">
         {PAGES.slice(1).map((page) => (
           <Link
@@ -29,6 +28,6 @@ export const Header = () => {
           </Link>
         ))}
       </div>
-    </div>
-  )
-}
+    </header>
+  );
+};
