b5f15eab7b5253d10d7656e6013ac0fd1f088c93
nedhmn <hermannned@gmail.com>
2025-05-26 21:35:42 -0400

    refactor: update work page tw dark mode utils

--- src/components/work-experience.tsx
+++ src/components/work-experience.tsx
@@ -6,13 +6,12 @@ export const WorkExperience = ({ work }: { work: WorkExperienceType }) => {
     <div className="relative">
       {/* Timeline Dot */}
       <div className="absolute top-1 -left-[30px] h-3 w-3 rounded-full bg-gray-400"></div>
+
       {/* Work Header */}
-      <h2 className="mt-8 mb-2 font-semibold text-gray-800 dark:text-zinc-200">
-        {work.role}
-      </h2>
+      <h2 className="mb-2 font-semibold">{work.role}</h2>
       <div className="flex flex-col gap-2 sm:flex-row sm:items-center sm:justify-between">
         <a
-          className="flex items-center text-blue-500 hover:text-blue-500 dark:text-gray-400 dark:underline dark:decoration-gray-800 dark:underline-offset-2 hover:dark:text-gray-300"
+          className="flex items-center text-blue-500 hover:text-blue-700"
           href={work.companyUrl}
           target="_blank"
           rel="noopener noreferrer"
@@ -22,25 +21,21 @@ export const WorkExperience = ({ work }: { work: WorkExperienceType }) => {
         </a>
         <div className="mb-1 flex items-center">
           <CalendarIcon size={14} className="mr-2" />
-          <span className="leading-snug text-gray-800 dark:text-zinc-300">
+          <span className="leading-snug text-primary-foreground">
             {work.startDate} - {work.endDate}
           </span>
         </div>
       </div>
 
       {/* Work Description */}
-      <p className="my-2 leading-snug text-gray-800 dark:text-zinc-300">
-        {work.description}
-      </p>
+      <p className="my-2 text-foreground">{work.description}</p>
 
       {/* Work Highlights */}
       <ul className="list-disc space-y-1 pl-5 text-gray-800 dark:text-zinc-300">
         {work.highlights.map((highlight, index) => (
-          <li key={index} className="pl-1">
+          <li key={index} className="pb-1 pl-1">
             {/* Description */}
-            <p className="leading-snug text-gray-800 dark:text-zinc-300">
-              {highlight.description}
-            </p>
+            <p className="l">{highlight.description}</p>
           </li>
         ))}
       </ul>
--- src/components/work-header.tsx
+++ src/components/work-header.tsx
@@ -1,4 +1,4 @@
-import { FileTextIcon } from 'lucide-react'
+import { ExternalLinkIcon } from 'lucide-react'
 
 export const WorkHeader = () => {
   return (
@@ -13,8 +13,8 @@ export const WorkHeader = () => {
         rel="noopener noreferrer"
         className="flex items-center gap-2 rounded-md bg-gray-200 px-4 py-2 text-sm text-gray-700 transition-colors hover:bg-gray-300 hover:text-gray-900 dark:bg-zinc-700 dark:text-zinc-300 dark:hover:bg-zinc-600 dark:hover:text-zinc-100"
       >
-        <FileTextIcon size={18} />
-        Download Resume
+        <ExternalLinkIcon size={18} />
+        Resume
       </a>
     </div>
   )
